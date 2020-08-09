f349954dadebd50890b076def558a381cb8ffa6b
Tanav-Mittal <68111690+Tanav-Mittal@users.noreply.github.com>
2020-08-09 20:45:20 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -6,14 +6,14 @@ class Ground {
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
-      this.x = x;
-      this.y = y;
+     // this.x = x;
+      //this.y = y;
       World.add(world, this.body);
     }
     display(){
     
       rectMode(CENTER);
       fill("yellow");
-      rect(this.x, this.y, this.width, this.height);
+      rect(this.body.position.x, this.body.position.y, this.width, this.height);
     }
   };
