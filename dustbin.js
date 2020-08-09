fb04f44b52f032cae8d1620205a8d74d4393e854
Tanav-Mittal <68111690+Tanav-Mittal@users.noreply.github.com>
2020-08-09 20:48:37 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -2,21 +2,22 @@ class Log
 {
     constructor(x, y, width, height) {
         var options = {
-            'restitution':0.8,
+            /*'restitution':0.8,
             'friction':1.0,
-            'density':1.0
+            'density':1.0*/
+            isStatic:true
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.x = x;
-        this.y = y;
+        //this.x = x;
+        //this.y = y;
         World.add(world, this.body);
       }
       display()
       {
         fill("white");
         rectMode(CENTER);
-        rect(this.x,this.y,this.width,this.height);
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
       }
 }
