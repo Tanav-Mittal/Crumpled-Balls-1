465e4eecf73969b37606422ef7530bb34905a549
Tanav-Mittal <68111690+Tanav-Mittal@users.noreply.github.com>
2020-08-14 21:10:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -51,7 +51,7 @@ function keyPressed()
 {
 	if (keyCode === UP_ARROW)
 	{
-		Body.applyForce(paper.body,paper.body.position,{x:300,y:-500})
+		Body.applyForce(paper.body,paper.body.position,{x:85,y:-300})
 	}
 
 }
